8ef1fa5216204d055d571366761eca9a86e3738c
sergeyt <stodyshev@gmail.com>
2014-02-22 16:35:53 +0700

    non-async simple fileset

--- core/fileset.fs
+++ core/fileset.fs
@@ -1,53 +1,55 @@
 ﻿module xake
 
 open System
+open System.Linq
 open System.IO
 
-let filter_files dir fn =
-  async {
-    let files = Directory.EnumerateFiles(dir, "*", SearchOption.AllDirectories)
-    let filtered = files |> Seq.filter fn
-    List.ofSeq filtered
-  }
+let private identity x = x
+let (+/) path1 path2 = Path.Combine(path1, path2)
 
-let glob_match path pattern =
-  // TODO complete
-  true
-
-// TODO make async and faster, maybe using regular expressions
 // globbing matcher - finds all files in given directory matched to specified pattern
-let glob pattern cwd = 
-  async {
-    let dir = if String.IsNullOrEmpty(cwd) then Environment.CurrentDirectory else cwd
-    // TODO get root dir
-    let root_dir = dir
-    filter_files root_dir, (fun (f) -> glob_match f pattern)
-  }
+let private glob (cwd:String) (pattern:String) = 
+  // utils
+  let search dir pattern all =
+    let opt = if all then SearchOption.AllDirectories else SearchOption.TopDirectoryOnly
+    let files = Directory.EnumerateFiles(dir, pattern, opt)
+    Seq.toList files
+  // use process current directory if cwd is not specified
+  let root_dir = if String.IsNullOrEmpty(cwd) then Environment.CurrentDirectory else cwd
+  let i = pattern.IndexOf('*')
+  if i >= 0 then // is pattern?
+    // extract search pattern
+    let i2 = pattern.LastIndexOf('/')
+    // TODO handle \ also
+    let search_pattern = if i2 >= 0 then pattern.Substring(i2 + 1) else pattern
+    // get root dir
+    let start_dir = pattern.Substring(0, i)
+    let dir = root_dir +/ start_dir
+    let all = i + 1 < pattern.Length && pattern.[i + 1] = '*'
+    search dir search_pattern all
+  else
+    // TODO ensure file exist
+    [pattern]
 
-let globs patterns cwd =
-  async {
-    let results = patterns |> List.map (fun (p) -> glob p cwd)
-    List.collect (fun x -> x), results
-  }
-
-let private startsWith prefix (s:String) = s.StartsWith(prefix)
-
-// TODO make async
+// TODO make async, try asyncSeq
 // fileset - returns files matches to given patterns
-let fileset pattern_list cwd = 
-  async {
-    // be safe, filter empty patterns
-    let patterns = pattern_list |> List.filter (String.IsNullOrEmpty >> not)
-    // get include patterns
-    let includes = patterns |> List.filter (startsWith "!" >> not)
-    // get exclude patterns
-    let excludes =
-      patterns
-        |> List.filter (startsWith "!")
-        |> List.map (fun (p) -> p.Substring(1))
-    // get all files
-    let all_files = globs includes cwd
-    let excluded_files = globs excludes cwd
-    // TODO subtract excluded_files
-    all_files
-  }
+let fileset patterns cwd = 
+  // utils
+  let startsWith prefix (s:String) = s.StartsWith(prefix)
+  let substr startIndex (s:String) = s.Substring(startIndex)
+  let exclude a b = Seq.toList(Enumerable.Except(a, b))
+  let globs patterns cwd =
+    let results = patterns |> List.map (glob cwd)
+    List.collect identity results
+  // use process current directory if cwd is not specified
+  let dir = if String.IsNullOrEmpty(cwd) then Environment.CurrentDirectory else cwd
+  // be safe, filter empty patterns
+  let pattern_list = patterns |> List.filter (String.IsNullOrEmpty >> not)
+  // get include patterns
+  let includes = pattern_list |> List.filter (startsWith "!" >> not)
+  // get exclude patterns
+  let excludes = pattern_list |> List.filter (startsWith "!") |> List.map (substr 1)
+  // get all files
+  let all_files = globs includes dir
+  let excluded_files = globs excludes dir
+  exclude all_files excluded_files
--- demo/build.fsx
+++ demo/build.fsx
@@ -1,4 +1,11 @@
-﻿#r "..\core\bin\Debug\xake.dll"
+﻿#r @"..\core\bin\Debug\xake.dll"
 open xake
+
+// dir of demo project
+let dir = @"c:\pets\xake\demo"
+
+// check fileset
+let files = fileset ["**/*.cs"] dir
+printfn "%A" files
 
 // TODO write build script for demo project
