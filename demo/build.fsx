e1cdd0ba48a5622342905a8064c0bfcec1259087
sergeyt <stodyshev@gmail.com>
2014-03-02 01:10:31 +0700

    minor

--- demo/build.fsx
+++ demo/build.fsx
@@ -1,5 +1,6 @@
 ﻿#r @"..\core\bin\Debug\xake.dll"
 open xake.fileset
+open xake.exec
 open xake.csc
 
 // dir of demo project
@@ -8,5 +9,8 @@ let dir = @"c:\pets\xake\demo"
 // check fileset
 let files = fileset ["**/*.cs"] dir
 printfn "%A" files
+
+// TODO check exec
+// exec("python.exe", [], "") |> ignore
 
 // TODO write build script for demo project
